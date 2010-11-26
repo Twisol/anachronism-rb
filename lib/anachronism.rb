e82cb420d7924958b4bb2cca9f0d28762b8f72e7
Twisol <twisolar@gmail.com>
2010-11-25 20:43:58 -0800

    Telnet stream parser is now working.

--- lib/anachronism.rb
+++ lib/anachronism.rb
@@ -1,29 +1,32 @@
-require 'telnet_parser'
+#!/usr/bin/env ruby
+
 
 module Anachronism
   VERSION = "0.0.1"
 end
-  
-  Event = Struct.new('Event', :type, :data)
-  
-  class Parser
-    def process (data)
-      [Event.new(:text, data)]
-    end
-  end
-end
+
+$:.unshift '../ext'
+require 'anachronism.so'
+
+data = "foo\r\n\r\0bar".force_encoding("ASCII-8BIT")
+data2 = "\xFF\xFF\xFF\xF6lolwat".force_encoding("ASCII-8BIT")
 
 telnet = Anachronism::Parser.new
-events = telnet.process("text")
+events = telnet.process(data)
+events.concat telnet.process(data2)
+
 events.each do |event|
+  puts event
   case event.type
   when :text
-    puts event.data
+    ;
   when :command
     ;
   when :option
     ;
   when :subnegotiation
+    ;
+  when :error
     ;
   else
     raise "Unknown event type: #{event.type}"
