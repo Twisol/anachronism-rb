a323aa550efb70da02e97b5c26ffced2a35133b9
Jonathan Castello <twisolar@gmail.com>
2011-01-17 02:56:25 -0800

    Condense NVT callbacks to just on_recv and on_send, passing an event structure to on_recv. Implement parser_send_subnegotiation(), and rename telnet_nvt_parse() to telnte_nvt_rect().

--- lib/anachronism/commands.rb
+++ lib/anachronism/commands.rb
@@ -1,9 +1,12 @@
 module Anachronism
-  COMMANDS = {
-    :NOP  => 241, :DM   => 242, :BRK => 243,
-    :IP   => 244, :AO   => 245, :AYT => 246,
-    :EC   => 247, :EL   => 248, :GA  => 249,
-    :WILL => 251, :WONT => 252, :DO  => 253,
-    :DONT => 254,
-  }
+  IAC = Hash.new {|h, k| k}
+  
+  [:SE,   :NOP, :DM,   :BRK,
+   :IP,   :AO,  :AYT,  :EC,
+   :EL,   :GA,  :SB,   :WILL,
+   :WONT, :DO,  :DONT, :IAC
+  ].each_with_index do |sym, i|
+    i += 240
+    IAC[sym], IAC[i] = i, sym
+  end
 end
--- lib/anachronism/nvt.rb
+++ lib/anachronism/nvt.rb
@@ -1,50 +1,11 @@
 module Anachronism
   class NVT
-    class Callbacks
-      attr_reader :nvt
-      
-      def initialize (nvt)
-        @nvt = nvt
-      end
-      
-      def on_text (text)
-        nvt.receive :text, text
-      end
-      
-      def on_command (command)
-        nvt.receive :command, command
-      end
-      
-      def on_option (option, command)
-        nvt.receive :option, option, command
-      end
-      
-      def on_mode (mode, extra)
-        nvt.receive :mode, mode, extra
-      end
-      
-      def on_error (fatal, message, position)
-        nvt.receive :error, fatal, message, position
-      end
-      
-      def on_send (data)
-        nvt.emit data
-      end
+    def initialize (receiver)
+      @nvt = Anachronism::Native.new(receiver)
     end
     
-    def initialize
-      @nvt = Anachronism::Native.new(Callbacks.new(self))
-    end
-    
-    def out (&blk)
-      @out = blk
-    end
-    
-    def process (data, &blk)
-      @receiver = blk
-      bytes_used = @nvt.process(data)
-      @receiver = nil
-      
+    def recv (data)
+      bytes_used = @nvt.recv(data)
       data[bytes_used..-1]
     end
     
@@ -53,30 +14,21 @@ module Anachronism
     end
     
     def send_command (command)
+      command = Anachronism::COMMANDS[command] if command.is_a? Symbol
       @nvt.send_command(command)
+    end
+    
+    def send_option (command, option)
+      command = Anachronism::COMMANDS[command] if command.is_a? Symbol
+      @nvt.send_option(command, option)
+    end
+    
+    def send_subnegotiation (option, data)
+      @nvt.send_subnegotiation(option, data)
     end
     
     def halt
       @nvt.halt
-    end
-    
-    def receive (type, *args)
-      @receiver.call(type, args) if @receiver
-    end
-    
-    def emit (data)
-      data = data.force_encoding("ASCII-8BIT")
-      @out.call(data) if @out
-      data
-    end
-    
-  private
-    def send_option (command, option)
-      unless (0..255).include?(option.ord)
-        raise "Option code must be within (0..255)"
-      end
-      
-      emit "\xFF#{command.chr}#{option.chr}"
     end
   end
 end
